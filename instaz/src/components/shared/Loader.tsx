c96e5e33c72e0608b740f1aa8d234e8168048cac
Brooklyn7x <126967831+Brooklyn7x@users.noreply.github.com>
2024-01-12 13:05:19 +0530

    Delete instaz directory

--- instaz/src/components/shared/Loader.tsx
+++
@@ -1,9 +0,0 @@
-const Loader = () => {
-  return (
-    <div className="flex justify-center items-center w-full">
-      <img src="" alt="loader" width={24} height={24} />
-    </div>
-  );
-};
-
-export default Loader;
